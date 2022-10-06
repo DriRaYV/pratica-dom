2c55c8e7c44db8bf6247ef48903c123729c3c498
DriRaYv <rafaellagdh55@gmail.com>
2022-10-05 21:09:05 -0300

    Adicionando correções

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
-let marca = "";
-let nome = "";
-let codigo = 0;
-let preco = 0;
-var arr = [];
-
-var obj = { marca: marca, nome: nome, codigo: codigo, preco: preco };
+var obj = { marca: "", nome: "", codigo: 0, preco: 0 };
 
 const listaSeara = document.getElementById("lista-seara");
 const listaSwift = document.getElementById("lista-swift");
 const listaFriboi = document.getElementById("lista-friboi");
 
 function addProduto() {
-  const optionMarca = document.getElementById("marca").value;
-  const nameProduto = document.getElementById("name").value;
-  const codigoProduto = document.getElementById("number").value;
-  const precoProduto = document.getElementById("preco").value;
-  obj.marca = optionMarca;
-  obj.nome = nameProduto;
-  obj.preco = precoProduto;
+  const optionMarca = document.getElementById("marca");
+  const nameProduto = document.getElementById("name");
+  const codigoProduto = document.getElementById("number");
+  const precoProduto = document.getElementById("preco");
+  obj.marca = optionMarca.value;
+  obj.nome = nameProduto.value;
+  obj.preco = precoProduto.value;
 
-  if (document.getElementById(`${codigoProduto}`) != null) {
+  if (document.getElementById(`${codigoProduto.value}`) != null) {
     alert("O código já existe! Insira outro.");
   } else {
-    obj.codigo = codigoProduto;
+    obj.codigo = codigoProduto.value;
     let confirmacao = confirm("Deseja adicionar");
-    if (obj.marca == "Seara") {
-      if (confirmacao == true) {
+    if (confirmacao == true) {
+      if (obj.marca == "Seara") {
         listaSeara.innerHTML += `<li id=${obj.codigo}>A marca é: ${obj.marca} O nome é: ${obj.nome} O código é:  ${obj.codigo} O preço é:  ${obj.preco}
           </li>`;
-      }
-    } else if (obj.marca == "Swift") {
-      if (confirmacao == true) {
+      } else if (obj.marca == "Swift") {
         listaSwift.innerHTML += `<li id=${obj.codigo}>A marca é: ${obj.marca} O nome é: ${obj.nome} O código é:  ${obj.codigo} O preço é:  ${obj.preco}
           </li>`;
-      }
-    } else if (obj.marca == "Friboi") {
-      if (confirmacao == true) {
+      } else if (obj.marca == "Friboi") {
         listaFriboi.innerHTML += `<li id=${obj.codigo}>A marca é: ${obj.marca} O nome é: ${obj.nome} O código é:  ${obj.codigo} O preço é:  ${obj.preco}
           </li>`;
       }
     }
+    optionMarca.value = null;
+    nameProduto.value = null;
+    codigoProduto.value = null;
+    precoProduto.value = null;
   }
 }
 function removeProduto() {
@@ -47,35 +41,25 @@ function removeProduto() {
   const liId = document.getElementById(`${listaID}`);
 
   let confirmacao = confirm("Deseja remover");
-  if (obj.marca == "Seara") {
-    if (confirmacao == true) {
-        if(liId == null){
-            alert('Não existe!')
-        }
-        else{
-            liId.remove();
-        }
-      
-    }
-  } else if (obj.marca == "Friboi") {
-    if (confirmacao == true) {
-        if(liId == null){
-            alert('Não existe!')
-        }
-        else{
-            liId.remove();
-        }
-      
-    }
-  } else if (obj.marca == "Swift") {
-    if (confirmacao == true) {
-        if(liId == null){
-            alert('Não existe!')
-        }
-        else{
-            liId.remove();
-        }
-      
+  if (confirmacao == true) {
+    if (obj.marca == "Seara") {
+      if (liId == null) {
+        alert("Não existe!");
+      } else {
+        liId.remove();
+      }
+    } else if (obj.marca == "Friboi") {
+      if (liId == null) {
+        alert("Não existe!");
+      } else {
+        liId.remove();
+      }
+    } else if (obj.marca == "Swift") {
+      if (liId == null) {
+        alert("Não existe!");
+      } else {
+        liId.remove();
+      }
     }
   }
 }
